d7b3abb9b9690d9de310103fe008b324c75ef22b
Toshtemir Chorshanbiyev <chorshanbiyevtoshtemur708@gmail.com>
2024-09-06 00:45:23 +0500

    Python-01-darsining kodi
    Bu mening birinchi python bo'yicha tushunchalarim !

---
+++ python-03.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 09:37:29 2024
+
+@author: Hp
+"""
+
+print( 'Salom Python ')
+#Bugun bizda yangi dars  bo'ladi!
+                   # python-03. Print(), Sinteks va arifmetik amallar.
+ #print() funktsiyasi  mtn yoki ifodalarni konsolga chiqarish vazifasini bajaradi. 
+print('Men kecha "Malika bozordan" noutbuk sotib oldim') 
+
+print(""" Bu dunyo o'tkinchi bu har doim unutma  ,
+# Hammasini unut va maqsading sari olg'a bor!""")  
+
+print(' Bu dunyo o\'tkinchi bu har doim unutma  , '
+      '\nHammasini unut va maqsading sari olg\'a bor!')        
+                               
+                                      #SYNTAX ERROR !    
+                                      #Arifmetik amallar.
+print(8+9*8)
+   #Bunda / bo'lish belgisi o'nlik sanoq sistemasida hisoblaydi!
+print(9/8)
+print(89/7) 
+print(20/5)
+#Agarda butun sonni olmoqchi bo'lsak // bo'lish belgisidan foydalanamiz!
+#Masalan.
+print(85//8)
+print(17//4) 
+#Sonlarni darajaga oshirish! Bunda ** orqali amalga oshiramiz!
+print(3**2)
+print(8**2)
+# Bo'linmaning qoldig'ini topishda % foiz belgisidan foydalanamiz!
+print(17%4)
+# yoki 
+print(81%6)
+print(25%5)
+
+# arifmetik ifodalarni  matn orqali ifodalash ! 
+print("Sakkizning kvadrati", 8**2 ,"ga tengdir! ")
+print('9+9=', 9*2)
+
+# Aylana uzunligini topish formulasi==L=2*p*r , p=3,14 .
+
+                            # UYGA VAZIFA ! AMALIYOT. 2024 .q
+print('"Nexia", "Tico", "Damas" ko\'rganlar qilar havas')
+
+# Misol va masalalar yechimi va berilishi .
+# 1). 5 ning 4-darajasini toping!
+print("Beshning 4-darajasi" , 5**4 , "ga teng")
+
+# 2). 22 ni 4 ga bo'lganda qancha qoldiq qoladi!
+print("Yigirma ikkini beshga bo'lganda qoldiq ", 22%5 ,'ga teng bo\'ladi.')
+
+# 3). Tomonlari 125 ga teng bo'lgan kvadratning yuzi va perimetrini toping .
+print( 'Tomonlari 125 ga teng bo\'lsa  kvadratning yuzi formulasi a kvadrat bo\'ladi ' 
+      , 125*125 ,'teng bo\'ladi',  
+      '\n perimetri bo\'lsa 4*a  bo\'ladi!)' ,4*125, 'teng bo\'ladi') 
+
+# 4). Diametri 12 ga teng bo'lgan doiraning yuzini topind.(p=3,14 deb oling.)
+print('Doira yuzini formulasi p*r**2 ga teng bo\'ladi bu yerda radius r=d/2 ga'
+      , 3.14*(12/2)**2, 'ga tengdir!')
+
+# 5). Katetlari 6 va 7 bo;lgan to'g'ri burchakli uchburchakning gipotenuzasini toping . Pifogor teoremasidan foydalaning!
+print('to\'g\'ri burchakli uchburchakning gipotenuzasi formulasi ikkila ' 
+      '\ntomonni kvadratlarining yig\'indising kvadrat ildiziga teng ya\'ni' 
+      '\nPifogor teoremasi', (6**2+7**2)**(1/2) )
